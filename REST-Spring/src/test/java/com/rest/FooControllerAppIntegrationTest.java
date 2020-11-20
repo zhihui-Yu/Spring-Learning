aa1374e62f39ba21f95e1cb15e9a2926d61e22f8
Simple <simpleyu@chancetop.com>
2020-11-20 18:23:59 +0800

    11-20 https://www.baeldung.com/building-a-restful-web-service-with-spring-and-java-based-configuration

---
+++ REST-Spring/src/test/java/com/rest/FooControllerAppIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * @author simple
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FooControllerAppIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void whenTestApp_thenEmptyResponse() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/foos"))
+            .andExpect(status().is4xxClientError());
+    }
+}
---
+++ Spring-Data-Mongo/src/main/java/com/mongo/SpringBootMonogApplication.java
@@ -0,0 +1,14 @@
+package com.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author simple
+ */
+@SpringBootApplication
+public class SpringBootMonogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMonogApplication.class, args);
+    }
+}
